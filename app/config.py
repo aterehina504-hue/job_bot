a0ec7e50ae1eeccbc16257610864ed3a4e90b65e
aterehina504-hue <aterehina504@gmail.com>
2026-02-03 15:25:39 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,14 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Telegram
+AZUR_JOB_BOT_TOKEN = os.getenv("AZUR_JOB_BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+# OpenAI
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Database
+DATABASE_URL = os.getenv("DATABASE_URL")
