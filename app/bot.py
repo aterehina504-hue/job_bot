e0b295f875b72291a07c986a63010c7564095250
aterehina504-hue <aterehina504@gmail.com>
2026-02-03 15:27:26 +0100

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,1 +1,24 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 
+from app.config import AZUR_JOB_BOT_TOKEN
+
+
+async def main():
+    bot = Bot(
+        token=AZUR_JOB_BOT_TOKEN,
+        parse_mode=ParseMode.HTML
+    )
+
+    dp = Dispatcher()
+
+    # handlers будем подключать на следующих шагах
+
+    print("🤖 Bot started")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
