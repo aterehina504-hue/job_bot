46d99c0b85034919142856de752d612a5e0411b8
aterehina504-hue <aterehina504@gmail.com>
2026-02-03 20:51:08 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -4,10 +4,10 @@ from app.db.database import engine
 from app.db.models import Base
 
 
-async def init_models():
+async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == "__main__":
-    asyncio.run(init_models())
+    asyncio.run(init_db())
