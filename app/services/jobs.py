877c8ccbfb8b7f552b57d0cc3af7606b84565933
aterehina504-hue <aterehina504@gmail.com>
2026-02-03 19:02:35 +0100

    Update jobs.py

--- app/services/jobs.py
+++ app/services/jobs.py
@@ -39,3 +39,36 @@ async def job_exists(
         )
     )
     return result.scalar_one_or_none() is not None
+
+async def save_job(
+    session: AsyncSession,
+    data: dict
+) -> Job | None:
+    """
+    Сохраняет вакансию в БД, если она не дубль
+    """
+    if await job_exists(
+        session,
+        title=data["title"],
+        city=data["city"],
+        contact=data["contact"]
+    ):
+        return None
+
+    job = Job(
+        title=data["title"],
+        category=data["category"],
+        city=data["city"],
+        job_type=data.get("job_type"),
+        salary=data.get("salary"),
+        short_description=data["short_description"],
+        full_description=data["full_description"],
+        contact=data["contact"],
+    )
+
+    session.add(job)
+    await session.commit()
+    await session.refresh(job)
+
+    return job
+
